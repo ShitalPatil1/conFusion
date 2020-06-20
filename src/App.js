1c80ed1e01c5e1defa922ef3decaec1ac253f161
Shital Patil <patilshital8844@gmail.com>
2020-06-20 23:37:51 +0530

    presentation and component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Navbar } from 'reactstrap';
-import Menu from './components/MenuComponent';
+import React, { Component } from 'react'
+import Main from './components/MainComponent';
 import './App.css';
 import NavbarBrand from 'reactstrap/lib/NavbarBrand';
 
-function App() {
+class App extends Component {
+ 
+  render()
+  {
   return (
     <div>
-      <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-    <Menu />
+    <Main />
     </div>
   );
+  }
 }
 
 export default App;
