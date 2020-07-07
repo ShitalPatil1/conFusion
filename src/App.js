34d0365c3cfb9965f9643e240e335e9d28c2c6f2
Shital Patil <patilshital8844@gmail.com>
2020-07-07 23:16:51 +0530

    Intro To Redux

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import Main from './components/MainComponent';
 import './App.css';
 import NavbarBrand from 'reactstrap/lib/NavbarBrand';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+
+const store = ConfigureStore();
+
 class App extends Component {
- 
+
   render()
   {
   return (
+    <Provider store={store}>
     <BrowserRouter>
     <div>
     <Main />
     </div>
     </BrowserRouter>
+    </Provider>
   );
   }
 }
